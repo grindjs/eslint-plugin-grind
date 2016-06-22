ba9367e0b7f730036850e81edf206fb1c88b59c5
shnhrrsn <shnhrrsn@gmail.com>
2016-06-21 23:30:27 -0400

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,2 @@
+# eslint-plugin-import-auto-name
+eslint plugin for https://github.com/shnhrrsn/babel-plugin-import-auto-name
---
+++ index.js
@@ -0,0 +1,57 @@
+var path = require('path');
+
+module.exports = {
+
+	environments: {
+		grind: {
+			globals: {
+				HttpError: false,
+
+				BadRequestError: false,
+				UnauthorizedError: false,
+				PaymentRequiredError: false,
+				ForbiddenError: false,
+				NotFoundError: false,
+				MethodNotAllowedError: false,
+				NotAcceptableError: false,
+				ProxyAuthenticationRequiredError: false,
+				RequestTimeoutError: false,
+				ConflictError: false,
+				GoneError: false,
+				LengthRequiredError: false,
+				PreconditionFailedError: false,
+				RequestEntityTooLargeError: false,
+				RequestURITooLongError: false,
+				UnsupportedMediaTypeError: false,
+				RequestedRangeNotSatisfiableError: false,
+				ExpectationFailedError: false,
+				EnhanceYourCalmError: false,
+				UnprocessableEntityError: false,
+				LockedError: false,
+				FailedDependencyError: false,
+				UnorderedCollectionError: false,
+				UpgradeRequiredError: false,
+				PreconditionRequiredError: false,
+				TooManyRequestsError: false,
+				RequestHeaderFieldsTooLargeError: false,
+				NoResponseError: false,
+				RetryWithError: false,
+				BlockedByWindowsParentalControlsError: false,
+				ClientClosedRequestError: false,
+				InternalServerErrorError: false,
+				NotImplementedError: false,
+				BadGatewayError: false,
+				ServiceUnavailableError: false,
+				GatewayTimeoutError: false,
+				HTTPVersionNotSupportedError: false,
+				VariantAlsoNegotiatesError: false,
+				InsufficientStorageError: false,
+				LoopDetectedError: false,
+				BandwidthLimitExceededError: false,
+				NotExtendedError: false,
+				NetworkAuthenticationRequiredError: false
+			}
+		}
+	}
+
+};
